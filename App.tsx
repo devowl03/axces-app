847e5ae2cef6f3c4ec5d1dc4871603a7345aa5d9
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2024-04-04 17:47:41 +0530

    clean

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {StatusBar, Text, useColorScheme, View} from 'react-native';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Onboarding from './src/component/onboarding/Onboarding';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
